f6489bd66a379c385d3496e65d0677093a07256d
Felix Chern <idryman@gmail.com>
2017-01-05 08:21:12 +0800

    tx atomic

---
+++ tx_atomic1.c
@@ -0,0 +1,31 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+#include <stdio.h>
+#include <pthread.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <stdbool.h>
+#include "op_atomic.h"
+
+a_uint32_t thread_count = 0;
+extern uint64_t counts[128];
+
+void setup(int num_threads) {}
+
+void* test(void *arg)
+{
+  uint64_t bound = *(uint64_t*)arg;
+  uint32_t tid = atomic_fetch_add_explicit(&thread_count, 1,
+                                           memory_order_relaxed);
+  uint32_t slot = tid % SLOT;
+  for (uint64_t i = 0; i < bound; i++)
+    {
+      __transaction_atomic
+        {
+          counts[slot]++;
+        }
+    }
+  return NULL;
+}
+
